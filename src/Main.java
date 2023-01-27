a9c96a8cc85fb6817dcc8b6938d52ece5d8ec265
Maxim-Ermakov <prospethee@gmail.com>
2023-01-27 19:09:47 +0300

    ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        ArrayList<Integer> number = new ArrayList<>();
+        number.add(10);
+        number.add(20);
+        number.add(30);
+
+        System.out.println("Элементов с списке: " + number.size());
+        System.out.println("Цикл For:");
+        for (int i = 0; i < number.size(); i++) {
+            System.out.println(number.get(i));
+        }
+        System.out.println("Цикл While:");
+        Iterator <Integer> iterator= number.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+        System.out.println("Улучшенный цикл For:");
+        for (Integer o : number) {
+            System.out.println(o);
+        }
     }
+
 }
